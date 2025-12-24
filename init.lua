cafba761a324c9ba8cf66e0d92489594b238b862
cjgnass <cjgnass@gmail.com>
2025-12-23 22:06:58 -0500

    12/23/2025

--- init.lua
+++ init.lua
@@ -671,17 +671,17 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
-        -- gopls = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
+        clangd = {},
+        gopls = {},
+        pyright = {},
+        rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
+        ts_ls = {},
         --
 
         lua_ls = {
@@ -773,9 +773,9 @@ require('lazy').setup({
         typescript = { 'prettierd', 'prettier' },
         typescriptreact = { 'prettierd', 'prettier' }, -- for .tsx
         javascriptreact = { 'prettierd', 'prettier' }, -- for .jsx
-        c = { 'clang_format' },
-        cpp = { 'clang_format' },
-        java = { 'clang_format' },
+        c = { 'clang-format' },
+        cpp = { 'clang-format' },
+        java = { 'clang-format' },
         rust = { 'rustfmt' },
         go = { 'goimports', 'gofmt' },
         sql = { 'sqlfluff' },
@@ -805,16 +805,18 @@ require('lazy').setup({
           -- `friendly-snippets` contains a variety of premade snippets.
           --    See the README about individual language/framework/plugin snippets:
           --    https://github.com/rafamadriz/friendly-snippets
-          -- {
-          --   'rafamadriz/friendly-snippets',
-          --   config = function()
-          --     require('luasnip.loaders.from_vscode').lazy_load()
-          --   end,
-          -- },
+          {
+            'rafamadriz/friendly-snippets',
+            config = function()
+              require('luasnip.loaders.from_vscode').lazy_load()
+            end,
+          },
         },
         opts = {},
       },
       'folke/lazydev.nvim',
+      'neovim/nvim-lspconfig',
+      'nvim-lua/plenary.nvim',
     },
     --- @module 'blink.cmp'
     --- @type blink.cmp.Config
@@ -881,12 +883,14 @@ require('lazy').setup({
       signature = { enabled = true },
     },
   },
+  -- {
+  --   'dark-orchid/neovim',
+  --   lazy = false,
+  --   priority = 1000,
+  --   opts = {},
+  -- },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+  {
     'folke/tokyonight.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
@@ -896,15 +900,44 @@ require('lazy').setup({
           comments = { italic = false }, -- Disable italics in comments
         },
       }
-
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
       vim.opt.termguicolors = true
       vim.cmd.colorscheme 'tokyonight-storm'
     end,
   },
-
+  -- {
+  --   'Kalidozza-theme/neovim',
+  --   priority = 1000, -- Make sure to load this before all the other start plugins.
+  --   config = function()
+  --     vim.opt.termguicolors = true
+  --     vim.cmd.colorscheme 'kalidozza'
+  --   end,
+  -- },
+  -- {
+  --   'DonJulve/NeoCyberVim',
+  --   priority = 1000, -- Make sure to load this before all the other start plugins.
+  --   config = function()
+  --     require('NeoCyberVim').setup {
+  --       transparent = false, -- Boolean: Sets the background to transparent
+  --       italics = {
+  --         comments = true, -- Boolean: Italicizes comments
+  --         keywords = true, -- Boolean: Italicizes keywords
+  --         functions = true, -- Boolean: Italicizes functions
+  --         strings = true, -- Boolean: Italicizes strings
+  --         variables = true, -- Boolean: Italicizes variables
+  --       },
+  --       overrides = {}, -- A dictionary of group names, can be a function returning a dictionary or a table.
+  --     }
+  --     vim.cmd.colorscheme 'NeoCyberVim'
+  --   end,
+  -- },
+  -- {
+  --   'chaserensberger/christmas.nvim',
+  --   priority = 1000, -- Make sure to load this before all the other start plugins.
+  --   config = function()
+  --     vim.opt.termguicolors = true
+  --     vim.cmd.colorscheme 'christmas'
+  --   end,
+  -- },
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -13,7 +13,55 @@ vim.keymap.set('n', '<leader>e', ':Neotree toggle<CR>', { noremap = true, silent
 vim.keymap.set('n', '<leader>n', ':bn<cr>')
 vim.keymap.set('n', '<leader>p', ':bp<cr>')
 vim.keymap.set('n', '<leader>x', ':bd<cr>')
+-- Toggle diagnostics
+local diagnostics_active = true
 
+vim.keymap.set('n', '<leader>td', function()
+  diagnostics_active = not diagnostics_active
+  if diagnostics_active then
+    vim.diagnostic.enable()
+  else
+    vim.diagnostic.disable()
+  end
+end, { desc = 'Toggle diagnostics' })
+-- Toggle center cursor
+local centered = false
+local group_name = 'KeepCentered'
+
+local function enable()
+  if centered then
+    print 'Centering: already ON'
+    return
+  end
+  centered = true
+  vim.api.nvim_create_augroup(group_name, { clear = true })
+  vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI', 'WinEnter', 'BufWinEnter', 'VimResized' }, {
+    group = group_name,
+    callback = function()
+      vim.cmd 'silent! normal! zz'
+    end,
+  })
+  print 'Centering: ON'
+end
+
+local function disable()
+  if not centered then
+    print 'Centering: already OFF'
+    return
+  end
+  centered = false
+  pcall(vim.api.nvim_del_augroup_by_name, group_name)
+  print 'Centering: OFF'
+end
+
+local function toggle()
+  if centered then
+    disable()
+  else
+    enable()
+  end
+end
+vim.keymap.set('n', '<leader>z', toggle, { desc = 'Toggle cursor centering' })
 -- Custom plugins
 return {
   {
